17373035484a2732c1982bd4726e8d99f04bb16b
Millena Ferreira <millenamills44@gmail.com>
2025-05-21 20:56:49 -0300

    feat(types/Product): adds Product type to represent product data.

---
+++ src/types/Product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price_in_cents: number;
+  image_url: string;
+  category: string;
+  sales: number;
+  created_at: string;
+};
