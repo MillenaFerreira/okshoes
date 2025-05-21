bc4f6b2d636a9e9dbed743c08ed90cbbd896a9ad
Millena Ferreira <millenamills44@gmail.com>
2025-05-21 14:35:38 -0300

    feat(/app) - Using the Product List.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 
 import { AppHeader } from "@/components/AppHeader";
 import NavMenu from "@/components/NavMenu";
+import ProductGrid from "@/components/ProductGrid";
+import { products } from "@/data/products";
+
 
 
 export default function Home() {
@@ -9,6 +12,7 @@ export default function Home() {
     <>
       <AppHeader />
       <NavMenu />
+      <ProductGrid products={products} />
     </>
   );
 }
